a4c521864c6c6a13cc5ad80c9eb8f237656f8871
Max Nerius <nermax03@gmail.com>
2022-03-10 20:15:36 +0100

    implemented correction logic

--- Wordle.hs
+++ Wordle.hs
@@ -3,6 +3,7 @@ module Wordle
 , Wordle
 , wordleFromStr
 , correct
+, check
 ) where
 
 import qualified Data.List as List
@@ -18,12 +19,21 @@ wordleFromStr str = enumerate str
 
 -- compute the correctness of a given wordle.
 correct :: Wordle -> Wordle -> [Correctness]
-correct guess solution = correct' guess solution []
+correct guess solution = correct' checked solution []
+    where checked = check guess solution []
 
--- compute the correctness of a given wordle.
-correct' :: Wordle -> Wordle -> [Correctness] -> [Correctness]
-correct' []     _    acc = acc
-correct' (g:gs) s@(f:sltn) acc
-    | g == f          = correct' gs sltn          (Correct : acc)
-    | g `elemOf` s    = correct' gs (remove' g s) (Misplaced : acc) -- TODO: we cant just remove any equal letter!
-    | otherwise       = correct' gs s             (Wrong : acc)
+data Check = MisplacedOrBad (Char,Int) | Good deriving (Show, Eq)
+
+check :: Wordle -> Wordle -> [Check] -> [Check]
+check []     _          acc = acc
+check (g:gs) s@(f:sltn) acc
+    | g == f          = check gs sltn          (acc ++ [Good])
+    | otherwise       = check gs s             (acc ++ [MisplacedOrBad g])
+
+correct' :: [Check] -> Wordle -> [Correctness] -> [Correctness]
+correct' []      _    acc = acc
+correct' (c:chk) sltn acc = 
+    case c of Good                  -> correct' chk sltn (acc ++ [Correct])
+              MisplacedOrBad mob
+                | mob `elemOf` sltn -> correct' chk (remove' mob sltn) (acc ++ [Misplaced])
+                | otherwise         -> correct' chk sltn               (acc ++ [Wrong])
