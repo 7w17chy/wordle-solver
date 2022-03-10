651d3a21afb49cf3ccddc2deb4b0e0cfcd587364
Max Nerius <nermax03@gmail.com>
2022-03-10 20:45:28 +0100

    added accidentally

--- Test.hs
+++
@@ -1,8 +0,0 @@
-module Test
-( unzip'
-) where
-
-unzip' :: [(a,b)] -> ([a],[b])
-unzip' lst = unzip' lst ([],[])
-    where unzip' []         res   = res
-          unzip' ((e,i):ll) (a,b) = unzip' ll ((e:a),(i:b))
